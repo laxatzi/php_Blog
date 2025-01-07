b5b99307d4fd919c29df5197679bf904354c72b4
laxatzi <laxatzi@yahoo.com>
2025-01-07 14:40:26 +0200

    Move js dashboard file to js folder

--- public/admin.php
+++ public/admin.php
@@ -470,5 +470,5 @@
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
 
-    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script></body>
+    <script src="assets/js/dashboard.js"></script></body>
 </html>
